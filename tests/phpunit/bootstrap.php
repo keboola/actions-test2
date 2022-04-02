1386a3800b513b8eb717aa7d0f8b6edc283f1b3f
odin <odinuv@gmail.com>
2022-04-03 01:20:22 +0200

    test

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 var_dump('Variable');
-var_dump(getenv('TEST_VAR'));
+var_dump(strrev((string) getenv('TEST_VAR')));
 var_dump('End');
 
 require __DIR__ . '/../../vendor/autoload.php';
